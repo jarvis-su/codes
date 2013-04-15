5c04aa17b4d30b43261b967eb39d3be577572948
mail2sujie <mail2sujie@gmail.com>
2013-04-15 16:14:53 +0800

    t

---
+++ jarvisCode/src/main/java/utils/ReadPropertity.java
@@ -0,0 +1,18 @@
+package utils;
+
+import java.io.IOException;
+import java.util.Properties;
+public class ReadPropertity {
+        static Properties props = new Properties();
+        static {
+                try {
+                        props.load(ReadPropertity.class.getClassLoader().getResourceAsStream(
+                                        "utils.properties"));
+                } catch (IOException e1) {
+                        e1.printStackTrace();
+                }
+        }
+        public static String getProperty(String key) {
+                return props.getProperty(key);
+        }
+}
