6e5947806ecec7c2e4c7b7c3ad3bff52447a0ce6
jarvis-su001 <jarvis_su@126.com>
2013-05-20 15:41:56 +0800

    y

---
+++ Python3/src/html/test.py
@@ -0,0 +1,21 @@
+'''
+Created on May 20, 2013
+
+@author: C5023792
+'''
+
+
+from bs4 import BeautifulSoup
+import re
+
+doc = ['<html><head><title>Page title</title></head>',
+       '<body><p id="firstpara" align="center">This is paragraph <b>one</b>.',
+       '<p id="secondpara" align="blah">This is paragraph <b>two</b>.',
+       '</html>']
+soup = BeautifulSoup(''.join(doc))
+
+print(soup.prettify())
+
+if __name__ == '__main__':
+    pass
+print('ddddddddddd')
