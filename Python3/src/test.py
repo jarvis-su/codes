21012f6fb1ee22a923d9eb7cb97fa615cb64dfbf
Jarvis <Jarvis@Jarvis-PC>
2013-05-27 21:00:03 +0800

    t

--- Python3/src/test.py
+++ Python3/src/test.py
@@ -7,6 +7,7 @@ import webbrowser
 import os 
 
 import htmlTest.LoginJtrac
+import DBOperation.sqliteOpertion
 
 if __name__ == '__main__':
     pass
@@ -15,10 +16,14 @@ print('hello')
 # webbrowser.open('http://kbserver/workflow/app/')
 # webbrowser.open('http://kbserver/workflow/app/item/TSGPRD-54897/')
 
-user = 'jsu'
-pwd = 'sujiesujie'
-url ='http://kbserver/workflow/app/item/TSGPRD-54897/'
-t = htmlTest.LoginJtrac.loginJtracAndGet(user, pwd, url)
+# user = 'jsu'
+# pwd = 'sujiesujie'
+# url ='http://kbserver/workflow/app/item/TSGPRD-54897/'
+# t = htmlTest.LoginJtrac.loginJtracAndGet(user, pwd, url)
 
-print(t)
+# print(t)
+
+print(DBOperation.sqliteOpertion.getData('t'))
+
+
 
