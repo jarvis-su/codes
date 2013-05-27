d1f787199740eb45d8e248eb6e57e09a48e9cd93
jarvis-su001 <jarvis_su@126.com>
2013-05-27 16:34:08 +0800

    e

--- Python3/src/htmlTest/processData.py
+++ Python3/src/htmlTest/processData.py
@@ -3,6 +3,7 @@ Created on May 27, 2013
 
 @author: C5023792
 '''
+import sqlite3
 import htmlTest.processPageInfo
 
 if __name__ == '__main__':
@@ -11,4 +12,17 @@ if __name__ == '__main__':
 Ticket_id = 'TSGPRD-56019'
 url ='http://kbserver/workflow/app/item/' + Ticket_id
 
+conn = sqlite3.connect('D:/data/sqlite3/jtrac')
+
+def clearExistedData(conn, ticket_id):
+    sql = 'delete from info where trim(ticket_id) = \'' + ticket_id + '\'';
+    print(sql)
+    c = conn.cursor()
+    c.execute(sql)
+    conn.commit()
+    c.close()
+
+
+
 htmlTest.processPageInfo.getPageInfoAndStore(Ticket_id, url)
+clearExistedData(conn, Ticket_id)
