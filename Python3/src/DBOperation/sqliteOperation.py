45510b368354c4ac2a27209c4664f2063ff37461
jarvis-su001 <jarvis_su@126.com>
2013-05-28 10:05:48 +0800

    t

--- Python3/src/DBOperation/sqliteOperation.py
+++ Python3/src/DBOperation/sqliteOperation.py
@@ -5,8 +5,17 @@ import sqlite3
 def getConnetion():
     return sqlite3.connect('D:/data/sqlite3/jtrac')
 
-def getData(table_name):
+def getAllData(table_name):
     sql = 'select * from ' + table_name
+    conn = getConnetion()
+    cursor = conn.cursor()
+    rec = cursor.execute(sql)
+    result = rec.fetchall()
+    for r in result:
+        print(r);
+    return result
+
+def getAllDataBySql(sql):
     conn = getConnetion()
     cursor = conn.cursor()
     rec = cursor.execute(sql)
