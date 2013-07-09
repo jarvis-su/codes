bda0018a8202904910c6c28bb4b0e72687531279
jarvis-su001 <jarvis_su@126.com>
2013-07-09 16:34:04 +0800

    t

---
+++ dataOperation/src/test/java/HelloBean.java
@@ -0,0 +1,12 @@
+public class HelloBean {
+
+	private String helloWorld;
+
+	public String getHelloWorld() {
+		return helloWorld;
+	}
+
+	public void setHelloWorld(String helloWorld) {
+		this.helloWorld = helloWorld;
+	}
+}
